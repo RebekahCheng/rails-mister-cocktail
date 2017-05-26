2ced4711995ed895228046bbd858c2585da029a2
Rebekah Cheng <work@rebekahcheng.com>
2017-05-26 16:01:12 +0100

    made everything pink

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,6 +2,8 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 
+  has_attachment :photo
+
   CATEGORIES = [ 'Long Island Iced Tea', 'White Russian', 'Whiskey Sour','Gin & Tonic', 'Martini', 'Negroni', 'Bloody Mary', 'Margarita', 'Daiquiri', 'Mojito', 'Cosmopolitan']
 
   validates :name, presence: true, uniqueness: true
